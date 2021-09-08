26e2b5d82c0dd4c9ad759eeb75224293c92af49c
Roda <55464245+roda37@users.noreply.github.com>
2021-09-08 11:25:06 +0200

    Create brightness.py

---
+++ brightness.py
@@ -0,0 +1,26 @@
+import os
+import getch
+
+print('1 to raise, 2 to lower')
+while True:
+    getch_input = getch.getch()
+    if getch_input == '1':
+        try:
+            open('log', 'w')
+        except FileNotFoundError:
+            os.system('touch log')
+        reading_brightness = os.system('cat /sys/class/backlight/intel_backlight/brightness >> log')
+        with open('log', 'r') as human:
+            s = int(human.read())
+            multiply = s + 200
+            os.system('echo ' + str(multiply) + ' >> /sys/class/backlight/intel_backlight/brightness')
+    elif getch_input == '2':
+        try:
+            open('log', 'w')
+        except FileNotFoundError:
+            os.system('touch log')
+        reading_brightness = os.system('cat /sys/class/backlight/intel_backlight/brightness >> log')
+        with open('log', 'r') as human:
+            s = int(human.read())
+            multiply = s - 200
+            os.system('echo ' + str(multiply) + ' >> /sys/class/backlight/intel_backlight/brightness')
